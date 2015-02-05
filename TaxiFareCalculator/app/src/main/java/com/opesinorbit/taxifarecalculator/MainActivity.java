7e04b6e8852e8d48a60d5f903daddd32924a727a
jcopperman <opperman.jonathan@gmail.com>
2015-02-05 09:30:25 +0200

    test 2

--- TaxiFareCalculator/app/src/main/java/com/opesinorbit/taxifarecalculator/MainActivity.java
+++ TaxiFareCalculator/app/src/main/java/com/opesinorbit/taxifarecalculator/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         createMapView();
-        addMarker()//removed ;
+        addMarker()
     }
 
     /**
